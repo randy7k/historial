28c6d664cfe233359ec76aa268c9ae70c3d8886f
Randy Rivera <randyr7k@gmail.com>
2020-12-05 00:48:32 -0400

    add 'Objetivo' and 'Consecucion' models

--- src/admin.py
+++ src/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from src.models import Objetivo, Consecucion
 
-# Register your models here.
+admin.site.register(Objetivo)
+admin.site.register(Consecucion)
--- src/models.py
+++ src/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Objetivo(models.Model):
+    metrica = models.CharField(blank=False, max_length=30)
+    descripcion = models.TextField(blank=False, max_length=150)
+
+    def __str__(self):
+        return self.metrica
+
+class Consecucion(models.Model):
+    descripcion = models.CharField(blank=False, max_length=30)
+    meta = models.FloatField(blank=False)
+    porcentaje_de_consecucion = models.FloatField(blank=False)
+    objetivo = models.ForeignKey(Objetivo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descripcion + " (" + str(self.objetivo) + ")"
