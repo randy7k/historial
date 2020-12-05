49dca8eb02ab397b29e5b77fe36efe4799b286d1
Randy Rivera <randyr7k@gmail.com>
2020-12-04 23:53:47 -0400

    add src template

--- historial/urls.py
+++ historial/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from django.conf.urls import url,include
+from src import views
+from src import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^$',views.index,name='index'),
 ]
